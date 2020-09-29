c9311c42c1daaa885de7b02d386da0c7071040ba
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-29 03:02:52 -0400

    added gradient to header and pic

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,13 @@ class Landing extends Component {
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
-                <Cell col={12}></Cell>
+                <Cell col={12}>
+                <img
+                    src="https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/15107468_994850563993986_1244574238040743004_n.jpg?_nc_cat=101&_nc_sid=174925&_nc_ohc=M8c2tW6jti8AX8irb8x&_nc_ht=scontent-bos3-1.xx&oh=0b60242fb37076302b7146f36f132bfd&oe=5F995F8B"
+                    alt="avatar"
+                    className="avatar-img"
+                />
+                </Cell>
                 </Grid>
             </div>
         )
