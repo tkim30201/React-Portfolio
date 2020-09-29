46cbcf3fcc4acfbfd79cec22fa4592f59beff983
tkim30201 <63823948+tkim30201@users.noreply.github.com>
2020-09-29 03:37:18 -0400

    added small bio and areas for social

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -13,6 +13,20 @@ class Landing extends Component {
                     alt="avatar"
                     className="avatar-img"
                 />
+                <div className="banner-text">
+                    <h1>Student</h1>
+                    <hr/>
+                    <p>HTML/CSS | Bootstrap | React | NodeJS | Express</p>
+                    <div className="social-links">
+                        <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                        </a>
+
+                        <a href="https://github.com/tkim30201" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-fa-github-square" aria-hidden="true" />
+                        </a>
+                    </div>
+                </div>
                 </Cell>
                 </Grid>
             </div>
